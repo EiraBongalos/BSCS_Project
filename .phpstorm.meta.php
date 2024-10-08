1958626cc765c76f3ba440e095355e83b496106f
EiraBongalos <bongaloseira@gmail.com>
2024-10-08 17:39:05 +0800

    Add files via upload
    commit

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -1,11 +1,10 @@
 <?php
+namespace PHPSTORM_META {
+    registerArgumentsSet("date_units", "millenania", "millennium", "century", "centuries", "decade", "decades", "year", "years", "y", "yr", "yrs", "quarter", "quarters", "month", "months", "mo", "mos", "week", "weeks", "w", "day", "days", "d", "hour", "hours", "h", "minute", "minutes", "m", "second", "seconds", "s", "millisecond", "milliseconds", "milli", "ms", "microsecond", "microseconds", "micro", "µs");
+    expectedArguments(\Carbon\Traits\Units::add(), 0, argumentsSet("date_units"));
+    expectedArguments(\Carbon\Traits\Units::add(), 1, argumentsSet("date_units"));
+    expectedArguments(\Carbon\CarbonInterface::add(), 0, argumentsSet("date_units"));
+    expectedArguments(\Carbon\CarbonInterface::add(), 1, argumentsSet("date_units"));
 
-namespace PHPSTORM_META;
-
-override(\Mockery::mock(0), map(["" => "@"]));
-override(\Mockery::spy(0), map(["" => "@"]));
-override(\Mockery::namedMock(0), map(["" => "@"]));
-override(\Mockery::instanceMock(0), map(["" => "@"]));
-override(\mock(0), map(["" => "@"]));
-override(\spy(0), map(["" => "@"]));
-override(\namedMock(0), map(["" => "@"]));
+    expectedArguments(\Carbon\CarbonInterface::getTimeFormatByPrecision(), 0, "minute", "second", "m", "millisecond", "µ", "microsecond", "minutes", "seconds", "ms", "milliseconds", "µs", "microseconds");
+}
